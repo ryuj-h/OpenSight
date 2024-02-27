291b00ed20eae90f406f91039f0722aa122eadb3
류진호 <dbwlsgh57@naver.com>
2024-02-27 17:53:35 +0900

    feat : 얼굴 인식 기능 구현 완료

--- src/test/java/org/example/b104/B104ApplicationTests.java
+++ src/test/java/org/example/b104/B104ApplicationTests.java
@@ -1,13 +1,17 @@
 package org.example.b104;
 
+import org.example.b104.global.util.AmazonRekognitionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class B104ApplicationTests {
 
+    AmazonRekognitionService rekognitionService = new AmazonRekognitionService();
     @Test
     void contextLoads() {
+        System.out.println("#######################################contextLoads");
+        rekognitionService.detectFaces("c:/mypic.png");
     }
 
 }
