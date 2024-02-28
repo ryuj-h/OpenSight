5c2e412684b20e719f7010eb7ccfc2fd121e08f8
혜지 <hms7248@naver.com>
2024-02-28 16:37:40 +0900

    :sparkles: OauthService 추가

---
+++ src/main/java/org/example/b104/oauth2/sevice/OauthService.java
@@ -0,0 +1,97 @@
+package org.example.b104.oauth2.sevice;
+
+import lombok.RequiredArgsConstructor;
+import org.example.b104.domain.user.entity.User;
+import org.example.b104.domain.user.repository.UserRepository;
+import org.example.b104.oauth2.*;
+import org.example.b104.oauth2.response.LoginResponse;
+import org.example.b104.oauth2.response.OauthTokenResponse;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class OauthService {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final InMemoryProviderRepository inMemoryProviderRepository;
+
+    private final UserRepository userRepository;
+    public LoginResponse login(String providerName, String code) {
+
+        // 프론트에서 providerName 받아 InMemoryProviderRepository에서 OauthPRovider 가져오기
+        OauthProvider provider = inMemoryProviderRepository.findByProviderName(providerName);
+
+        // 1. accessToken 가져오기
+        OauthTokenResponse tokenResponse = getToken(code, provider);
+        // 2. 유저 정보 가져오기
+        UserProfile userProfile = getUserProfile(providerName, tokenResponse, provider);
+
+        // 3. 유저 DB에 저장
+        User user = saveOrUpdate(userProfile);
+
+        // JWT 토큰 만들기
+//        String accessToken
+        return null;
+    }
+
+    // 가져온 유저 정보 DB에 저장
+    private UserProfile getUserProfile(String providerName, OauthTokenResponse tokenResponse, OauthProvider provider) {
+        Map<String, Object> userAttributes = getUserAttributes(provider, tokenResponse);
+        // 유저 정보(map)를 통해 UserProfile 만들기
+        return OauthAttributes.extract(providerName, userAttributes);
+    }
+
+    // OAuth 서버에서 유저 정보 map으로 가져오기
+    private Map<String, Object> getUserAttributes(OauthProvider provider, OauthTokenResponse tokenResponse) {
+        return  WebClient.create()
+                .get()
+                .uri(provider.getUserInfoUrl())
+                .headers(header -> header.setBearerAuth(tokenResponse.getAccessToken()))
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
+                .block();
+    }
+
+    private OauthTokenResponse getToken(String code, OauthProvider provider) {
+        return WebClient.create()
+                .post()
+                .uri(provider.getTokenUrl())
+                .headers(header -> {
+                    header.setBasicAuth(provider.getClientId(), provider.getClientSecret());
+                    header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+                    header.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+                    header.setAcceptCharset(Collections.singletonList(StandardCharsets.UTF_8));
+                })
+                .bodyValue(tokenRequest(code, provider))
+                .retrieve()
+                .bodyToMono(OauthTokenResponse.class)
+                .block();
+    }
+
+    private MultiValueMap<String, String> tokenRequest(String code, OauthProvider provider) {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("code", code);
+        formData.add("grant_type", "authorization_code");
+        formData.add("redirect_uri", provider.getRedirectUrl());
+        return formData;
+    }
+
+    private User saveOrUpdate(UserProfile userProfile) {
+        User user = userRepository.findByOauthId(userProfile.getOauthId())
+                .map(entity -> entity.update(
+                        userProfile.getName(), userProfile.getEmail()))
+                .orElseGet(userProfile::toUser);
+    return userRepository.save(user);
+    }
+}
