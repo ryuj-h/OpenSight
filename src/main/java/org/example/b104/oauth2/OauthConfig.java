230916db3394b63f70823ea9ab355ef248c209fe
혜지 <hms7248@naver.com>
2024-02-28 11:12:40 +0900

    :sparkles: Oauth 객체 저장

--- src/main/java/org/example/b104/oauth2/OauthConfig.java
+++ src/main/java/org/example/b104/oauth2/OauthConfig.java
@@ -2,11 +2,21 @@ package org.example.b104.oauth2;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableConfigurationProperties
 public class OauthConfig {
-    private final OauthProperties oauthProperties;
+
+    private final OauthProperties properties;
+
+    @Bean
+    public InMemoryProviderRepository inMemoryProviderRepository() {
+        Map<String, OauthProvider> providers = OauthAdapter.getOauthProviders(properties);
+        return new InMemoryProviderRepository(providers);
+    }
 }
