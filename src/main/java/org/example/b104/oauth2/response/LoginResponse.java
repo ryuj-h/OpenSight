23c2b5fc2d43a209bcf01819de1f4fd594cd2215
혜지 <hms7248@naver.com>
2024-03-04 17:51:55 +0900

    :sparkles: Response에 jwt토큰 추가

--- src/main/java/org/example/b104/oauth2/response/LoginResponse.java
+++ src/main/java/org/example/b104/oauth2/response/LoginResponse.java
@@ -12,16 +12,18 @@ public class LoginResponse {
     private String nickname;
     private String tokenType;
     private String accessToken;
+    private String jwtToken;
     private String refreshToken;
 
     @Builder
-    public LoginResponse(Long id, String name, String email, String nickname, String tokenType, String accessToken, String refreshToken) {
+    public LoginResponse(Long id, String name, String email, String nickname, String tokenType, String accessToken, String jwtToken, String refreshToken) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.nickname = nickname;
         this.tokenType = tokenType;
         this.accessToken = accessToken;
+        this.jwtToken = jwtToken;
         this.refreshToken = refreshToken;
     }
 }
