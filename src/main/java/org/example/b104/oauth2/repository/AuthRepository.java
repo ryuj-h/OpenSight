705ef237a39bcc0fde0dc622b921273c16f05993
혜지 <hms7248@naver.com>
2024-03-04 17:51:11 +0900

    :bug: AuthRepository 에러 수정

--- src/main/java/org/example/b104/oauth2/repository/AuthRepository.java
+++ src/main/java/org/example/b104/oauth2/repository/AuthRepository.java
@@ -1,5 +1,7 @@
 package org.example.b104.oauth2.repository;
 
+import org.example.b104.domain.user.entity.User;
+import org.example.b104.oauth2.UserProfile;
 import org.example.b104.oauth2.entity.Auth;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +10,6 @@ import java.util.Optional;
 
 @Repository
 public interface AuthRepository extends JpaRepository<Auth, Long> {
-    Optional<Auth> findUserId(Long userId);
+    Optional<Auth> findByUserId(Long userId);
+
 }
