0f1dccceafc39e7d69a9870f2f8c3201112a7fd6
혜지 <hms7248@naver.com>
2024-03-06 13:34:41 +0900

    :art: OauthController 수정

--- src/main/java/org/example/b104/oauth2/controller/OauthController.java
+++ src/main/java/org/example/b104/oauth2/controller/OauthController.java
@@ -3,23 +3,23 @@ package org.example.b104.oauth2.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.b104.oauth2.response.LoginResponse;
 import org.example.b104.oauth2.sevice.OauthService;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequiredArgsConstructor
-public class OauthRestController {
+@Controller
+public class OauthController {
 
     private final OauthService oauthService;
 
     @GetMapping("/login/oauth2/code/{provider}")
-    public ResponseEntity<LoginResponse> login(@PathVariable String provider, @RequestParam("code") String code) {
+    public String login(@PathVariable String provider, @RequestParam("code") String code) {
         System.out.println("provider : " + provider + ", code : " + code);
         LoginResponse loginResponse = oauthService.login(provider, code);
         System.out.println("longinResponse 토큰 : "+loginResponse.getJwtToken());
-        return ResponseEntity.ok().body(loginResponse);
+        String redirectUrl = "http://192.168.31.38:5173/main?accessToken=" + loginResponse.getJwtToken() + "&refreshToken=" + loginResponse.getRefreshToken();
+        return "redirect:" + redirectUrl;
     }
 }
