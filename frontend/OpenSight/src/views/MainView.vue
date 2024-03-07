ca05c18ae924299eb746ab1ab12cb0202345fbda
임태수 <ts980730@naver.com>
2024-03-07 16:02:39 +0900

    :sparkles: feat:소셜로그인 기능 페이지

---
+++ frontend/OpenSight/src/views/MainView.vue
@@ -0,0 +1,16 @@
+<script setup>
+function parseJwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload);
+};
+
+console.log(parseJwt(localStorage.getItem('accessToken')))
+</script>
+
+<template>
+  <h1>hi</h1>
+</template>
