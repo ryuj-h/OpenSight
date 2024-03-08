d46e323e0f721a40c9bd3aa5491bae6c7c4b4bf3
혜지 <hms7248@naver.com>
2024-03-08 17:37:16 +0900

    :bug: refactor: 로그인 요청 주소 whitelist에 추가

--- backend/src/main/java/org/example/b104/domain/oauth2/AuthenticationFilter.java
+++ backend/src/main/java/org/example/b104/domain/oauth2/AuthenticationFilter.java
@@ -15,7 +15,7 @@ public class AuthenticationFilter implements Filter {
     private final JwtTokenProvider jwtTokenProvider;
 
     // 인증 없이 접근 허용되는 페이지
-    private static final String[] whitelist = {"/public"};
+    private static final String[] whitelist = {"/public", "/login/oauth2/code","/main"};
 
     public AuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
