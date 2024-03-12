66150e486c58ee3bf463ee781ae4cec6e2a80c8b
혜지 <hms7248@naver.com>
2024-03-12 14:27:01 +0900

    :sparkles: feat: 일반 로그인 추가

--- backend/src/main/java/org/example/b104/domain/user/controller/UserController.java
+++ backend/src/main/java/org/example/b104/domain/user/controller/UserController.java
@@ -1,20 +1,32 @@
 package org.example.b104.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.b104.domain.oauth2.response.SocialLoginResponse;
+import org.example.b104.domain.user.controller.request.LoginRequest;
+import org.example.b104.domain.user.controller.response.LoginResponse;
+import org.example.b104.domain.user.service.UserService;
+import org.example.b104.global.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
+
+    final UserService userService;
     @GetMapping("/test")
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("test");
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<LoginResponse>> login(
+            @RequestBody LoginRequest request
+    ) {
+        return ResponseEntity.ok(ApiResponse.createSuccess(userService.Login(request.toLogin())));
+    }
+
 
 }
