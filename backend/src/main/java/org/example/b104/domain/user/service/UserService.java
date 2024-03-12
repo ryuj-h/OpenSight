05810d2e5fcb046b98e92200775e45cf48130186
혜지 <hms7248@naver.com>
2024-03-12 17:04:48 +0900

    :recycle: refactor : 회원가입 기능 추가

--- backend/src/main/java/org/example/b104/domain/user/service/UserService.java
+++ backend/src/main/java/org/example/b104/domain/user/service/UserService.java
@@ -3,11 +3,14 @@ package org.example.b104.domain.user.service;
 import com.sun.jdi.request.InvalidRequestStateException;
 import lombok.RequiredArgsConstructor;
 import org.example.b104.domain.oauth2.JwtTokenProvider;
+import org.example.b104.domain.user.controller.response.CreateUserResponse;
 import org.example.b104.domain.user.controller.response.LoginResponse;
 import org.example.b104.domain.user.entity.User;
 import org.example.b104.domain.user.repository.UserRepository;
+import org.example.b104.domain.user.service.command.CreateUserCommand;
 import org.example.b104.domain.user.service.command.LoginCommand;
 import org.example.b104.global.exception.EntityNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,13 +24,15 @@ public class UserService {
     @Transactional(readOnly = false)
     public LoginResponse Login(LoginCommand command) {
 
+        BCryptPasswordEncoder bCryptPasswordEncoder  = new BCryptPasswordEncoder();
+
         // 입력 유효성 검사
         if (command.getUserEmail() == null || command.getUserPassword() == null) {
             throw new InvalidRequestStateException("이메일과 비밀번호는 필수 입력값입니다.");
         }
 
         User user = userRepository.findByEmail(command.getUserEmail());
-        if (user == null || !user.getPassword().equals(command.getUserPassword())) {
+        if (user == null || !bCryptPasswordEncoder.matches(command.getUserPassword(), user.getPassword())) {
             throw new EntityNotFoundException("로그인 실패");
         }
 
@@ -37,12 +42,26 @@ public class UserService {
 
         return LoginResponse.builder()
                 .id(user.getId())
-                .name(user.getName())
+                .name(user.getUsername())
                 .email(user.getEmail())
                 .tokenType("Bearer")
                 .accessToken(jwtToken)
                 .refreshToken(refreshToken)
                 .build();
 
+    }
+
+    @Transactional(readOnly = false)
+    public CreateUserResponse createUser(CreateUserCommand command) {
+        BCryptPasswordEncoder bCryptPasswordEncoder  = new BCryptPasswordEncoder();
+        User newUser = User.createNewUser(
+                command.getEmail(),
+                bCryptPasswordEncoder.encode(command.getPassword()),
+                command.getUsername()
+        );
+        User result = userRepository.save(newUser);
+        return CreateUserResponse.builder()
+                .userId(result.getId())
+                .build();
     }
 }
