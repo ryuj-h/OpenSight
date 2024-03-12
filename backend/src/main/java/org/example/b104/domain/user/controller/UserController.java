732278c61c5b180110d1a24c706f1ed73851232d
혜지 <hms7248@naver.com>
2024-03-12 15:05:25 +0900

    :art: refactor: 로그인 리턴값 loginResponse로 수정

--- backend/src/main/java/org/example/b104/domain/user/controller/UserController.java
+++ backend/src/main/java/org/example/b104/domain/user/controller/UserController.java
@@ -2,9 +2,7 @@ package org.example.b104.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.b104.domain.oauth2.response.SocialLoginResponse;
-import org.example.b104.domain.user.controller.request.CreateUserRequest;
 import org.example.b104.domain.user.controller.request.LoginRequest;
-import org.example.b104.domain.user.controller.response.CreateUserResponse;
 import org.example.b104.domain.user.controller.response.LoginResponse;
 import org.example.b104.domain.user.service.UserService;
 import org.example.b104.global.response.ApiResponse;
@@ -30,8 +28,6 @@ public class UserController {
     ) {
         LoginResponse loginResponse = userService.Login(request.toLogin());
         return ResponseEntity.ok(ApiResponse.createSuccess(loginResponse));
-
-    }
 
     @PostMapping("/create")
     public ResponseEntity<ApiResponse<CreateUserResponse>> createUser(
