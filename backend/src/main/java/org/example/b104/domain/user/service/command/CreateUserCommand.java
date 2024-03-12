a2dc3475351ac11cbd817d1ef6c7fddc868899ae
혜지 <hms7248@naver.com>
2024-03-12 17:02:55 +0900

    :sparkles: feat : CreateUserCommand 추가

---
+++ backend/src/main/java/org/example/b104/domain/user/service/command/CreateUserCommand.java
@@ -0,0 +1,12 @@
+package org.example.b104.domain.user.service.command;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CreateUserCommand {
+    private String email;
+    private String password;
+    private String username;
+}
