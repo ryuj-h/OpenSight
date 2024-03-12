8ce06e02f207b92503ab10a1e34b48a87ef945b7
혜지 <hms7248@naver.com>
2024-03-12 14:23:17 +0900

    :sparkles: feat: EntityNotFoundException 추가

---
+++ backend/src/main/java/org/example/b104/global/exception/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package org.example.b104.global.exception;
+
+public class EntityNotFoundException extends BusinessException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
