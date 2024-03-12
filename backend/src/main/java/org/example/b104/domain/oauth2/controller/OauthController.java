4591d0169b7dab10cb49c0fe2060fe82657b9181
혜지 <hms7248@naver.com>
2024-03-12 14:27:30 +0900

    :art: refactor: 반환 값 SocialLoginResponse로 수정

--- backend/src/main/java/org/example/b104/domain/oauth2/controller/OauthController.java
+++ backend/src/main/java/org/example/b104/domain/oauth2/controller/OauthController.java
@@ -1,7 +1,7 @@
 package org.example.b104.domain.oauth2.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.example.b104.domain.oauth2.response.LoginResponse;
+import org.example.b104.domain.oauth2.response.SocialLoginResponse;
 import org.example.b104.domain.oauth2.sevice.OauthService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +15,9 @@ public class OauthController {
     private final OauthService oauthService;
 
     @GetMapping("/login/oauth2/code/{provider}")
-    public String login(@PathVariable String provider, @RequestParam("code") String code) {
+    public String socialLogin(@PathVariable String provider, @RequestParam("code") String code) {
         System.out.println("provider : " + provider + ", code : " + code);
-        LoginResponse loginResponse = oauthService.login(provider, code);
+        SocialLoginResponse loginResponse = oauthService.socialLogin(provider, code);
         System.out.println("longinResponse 토큰 : "+loginResponse.getJwtToken());
         String redirectUrl = "http://192.168.31.38:5173/main?accessToken=" + loginResponse.getJwtToken() + "&refreshToken=" + loginResponse.getRefreshToken();
         return "redirect:" + redirectUrl;
