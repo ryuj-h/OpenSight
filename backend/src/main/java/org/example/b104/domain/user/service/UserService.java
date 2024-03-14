f0b9c711d30ad2d8df63da72694d79d1308ff838
혜지 <hms7248@naver.com>
2024-03-14 22:20:51 +0900

    :Sparkles: feat : BankResponse 추가

--- backend/src/main/java/org/example/b104/domain/user/service/UserService.java
+++ backend/src/main/java/org/example/b104/domain/user/service/UserService.java
@@ -1,8 +1,12 @@
 package org.example.b104.domain.user.service;
 
 import com.sun.jdi.request.InvalidRequestStateException;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.example.b104.domain.oauth2.JwtTokenProvider;
+import org.example.b104.domain.oauth2.OauthProvider;
+import org.example.b104.domain.oauth2.UserProfile;
+import org.example.b104.domain.oauth2.response.OauthTokenResponse;
 import org.example.b104.domain.user.controller.response.CreateUserResponse;
 import org.example.b104.domain.user.controller.response.LoginResponse;
 import org.example.b104.domain.user.entity.User;
@@ -10,9 +14,17 @@ import org.example.b104.domain.user.repository.UserRepository;
 import org.example.b104.domain.user.service.command.CreateUserCommand;
 import org.example.b104.domain.user.service.command.LoginCommand;
 import org.example.b104.global.exception.EntityNotFoundException;
+import org.example.b104.global.response.ApiResponse;
+import org.example.b104.global.response.BankApiResponse;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @RequiredArgsConstructor
@@ -53,15 +65,57 @@ public class UserService {
 
     @Transactional(readOnly = false)
     public CreateUserResponse createUser(CreateUserCommand command) {
-        BCryptPasswordEncoder bCryptPasswordEncoder  = new BCryptPasswordEncoder();
-        User newUser = User.createNewUser(
-                command.getEmail(),
-                bCryptPasswordEncoder.encode(command.getPassword()),
-                command.getUsername()
-        );
-        User result = userRepository.save(newUser);
-        return CreateUserResponse.builder()
-                .userId(result.getId())
-                .build();
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+        System.out.println("======api통신 시작=========" + command.getEmail());
+        BankApiResponse responseEntity = WebClient.create()
+                .post()
+                .uri("https://finapi.p.ssafy.io/ssafy/api/v1/member/")
+                .bodyValue(new MemberRequest("d16009763e1e4c82a538f3c29a090513", command.getEmail()))
+                .retrieve()
+                .bodyToMono(BankApiResponse.class)
+                .block();
+        System.out.println("=====통신완료========");
+
+        // 응답 처리
+//        System.out.println(responseEntity.getPayload().getUserKey());
+        if (responseEntity != null) {
+            String userKey = responseEntity.getPayload().getUserKey();
+//            // userKey를 저장
+            User newUser = User.createNewUser(
+                    command.getEmail(),
+                    bCryptPasswordEncoder.encode(command.getPassword()),
+                    command.getUsername(),
+                    command.getPhone(),
+                    userKey
+            );
+//
+            userRepository.save(newUser);
+            return CreateUserResponse.builder()
+                    .userId(newUser.getId())
+                    .build();
+//        }
+        }
+
+
+//    private MultiValueMap<String, String> userRequest (String apiKey, String userId) {
+//        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+//        formData.add("apiKey", apiKey);
+//        formData.add("userId", userId);
+//        return formData;
+//    }
+        return null;
     }
+
+    @Data
+    static class MemberRequest {
+        private String apiKey;
+        private String userId;
+
+        public MemberRequest(String apiKey, String userId) {
+            this.apiKey = apiKey;
+            this.userId = userId;
+        }
+    }
+
 }
