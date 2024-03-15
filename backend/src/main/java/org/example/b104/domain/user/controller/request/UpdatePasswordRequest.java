e98b35d329f1d9f8f9f451afed720e83023c7a6a
혜지 <hms7248@naver.com>
2024-03-15 17:45:26 +0900

    :sparkles: feat: 회원정보 수정

---
+++ backend/src/main/java/org/example/b104/domain/user/controller/request/UpdatePasswordRequest.java
@@ -0,0 +1,8 @@
+package org.example.b104.domain.user.controller.request;
+
+import lombok.Data;
+
+@Data
+public class UpdatePasswordRequest {
+    private String password;
+}
---
+++ backend/src/main/java/org/example/b104/domain/user/service/command/UpdateUserCommand.java
@@ -0,0 +1,12 @@
+package org.example.b104.domain.user.service.command;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UpdateUserCommand {
+    private String email;
+    private String username;
+    private String password;
+}
