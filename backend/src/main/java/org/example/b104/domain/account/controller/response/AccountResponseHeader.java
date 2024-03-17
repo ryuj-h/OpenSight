8911f45bce8abedcec89a7db74c52647af24a24b
류진호 <dbwlsgh57@naver.com>
2024-03-17 23:36:58 +0900

    :sparkles: feat : 생성가능한 계좌 확인 메소드 구현

--- backend/src/main/java/org/example/b104/domain/account/controller/response/AccountResponseHeader.java
+++ backend/src/main/java/org/example/b104/domain/account/controller/response/AccountResponseHeader.java
@@ -1,2 +1,21 @@
-package org.example.b104.domain.account.controller.response;public class AccountResponseHeader {
+package org.example.b104.domain.account.controller.response;
+
+import lombok.*;
+
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AccountResponseHeader {
+    private String responseCode;
+    private String responseMessage;
+    private String apiName;
+    private String transmissionDate;
+    private String transmissionTime;
+    private String institutionCode;
+    private String apiKey;
+    private String apiServiceCode;
+    private String institutionTransactionUniqueNo;
 }
--- backend/src/main/java/org/example/b104/domain/account/controller/response/REC.java
+++ backend/src/main/java/org/example/b104/domain/account/controller/response/REC.java
@@ -1,2 +1,16 @@
-package org.example.b104.domain.account.controller.response;public class REC {
+package org.example.b104.domain.account.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class REC {
+    public String accountTypeUniqueNo;
+    public String bankCode;
+    public String bankName;
+    public String accountTypeCode;
+    public String accountTypeName;
+    public String accountName;
 }
