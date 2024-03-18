7bcfac290cad8084a53591b27a9e0fa6f74db400
임태수 <ts980730@naver.com>
2024-03-18 10:47:31 +0900

    :art: feat: token부분 변경

--- frontend/OpenSight/src/views/MainView.vue
+++ frontend/OpenSight/src/views/MainView.vue
@@ -1,23 +1,47 @@
 <script setup>
-function parseJwt(token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    return JSON.parse(jsonPayload);
-};
-
-const token = localStorage.getItem('accessToken')
-
-const headers = {
-  'Authorization': `Bearer ${token}`,
-  'Content-Type': 'application/json'
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
 }
 
-console.log(headers)
+// Extract tokens
+var accessToken = getQueryVariable('accessToken');
+var refreshToken = getQueryVariable('refreshToken');
 
-console.log(parseJwt(localStorage.getItem('accessToken')))
+// Check if tokens are found before saving
+if (accessToken && refreshToken) {
+    // Save tokens to localStorage
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('refreshToken', refreshToken);
+} else {
+    console.log('Tokens not found in query string');
+}
+// function parseJwt(token) {
+//     var base64Url = token.split('.')[1];
+//     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+//     var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+//       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+//     }).join(''));
+//     return JSON.parse(jsonPayload);
+// };
+
+// const token = localStorage.getItem('accessToken')
+
+// const headers = {
+//   'Authorization': `Bearer ${token}`,
+//   'Content-Type': 'application/json'
+// }
+
+// console.log(headers)
+
+// console.log(parseJwt(localStorage.getItem('accessToken')))
 </script>
 
 <template>
