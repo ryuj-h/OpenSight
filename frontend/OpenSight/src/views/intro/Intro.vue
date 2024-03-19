3c7f4722d8596cb39860a5a83eede17c253fb5b2
임태수 <ts980730@naver.com>
2024-03-20 00:26:51 +0900

    :sparkles: feat:인트로페이지 css제외 구현

---
+++ frontend/OpenSight/src/views/intro/Intro.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue';
+import Section1 from './Section1.vue';
+import Section2 from './Section2.vue';
+import Section3 from './Section3.vue';
+import Section4 from './Section4.vue';
+import Section5 from './Section5.vue';
+
+
+// 각 섹션 컴포넌트를 배열에 객체 형태로 참조합니다.
+const sections = ref([
+  { name: 'Section1', component: Section1 },
+  { name: 'Section2', component: Section2 },
+  { name: 'Section3', component: Section3 },
+  { name: 'Section4', component: Section4 },
+  { name: 'Section5', component: Section5 },
+]);
+</script>
+
+<template>
+  <div class="scroll-container">
+    <component class="scroll-section" v-for="section in sections" :key="section.name" :is="section.component" />
+  </div>
+</template>
+
+
+<style>
+.scroll-container {
+  height: 100vh;
+  overflow-y: scroll;
+  scroll-snap-type: y mandatory;
+}
+
+
+.scroll-section {
+  height: 100vh;
+  scroll-snap-align: start; /* 스크롤 섹션의 시작 부분에서 스냅 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* 필요한 추가 스타일링 */
+}
+/* 스크롤 섹션의 시작 부분에서 스냅할 컴포넌트 스타일링 */
+/* 컴포넌트별로 스타일을 설정할 경우, 각 컴포넌트 내부의 <style>에 작성합니다. */
+</style>
---
+++ frontend/OpenSight/src/views/intro/Section4.vue
@@ -0,0 +1,58 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="img-section">
+      <div class="left-section">
+        <p>음성을 통한</p>
+        <p>쉬운 업무 처리</p>
+        <!-- <img src="@/assets/img/intro-1.png" alt="전맹시각장애인이 보는 화면 이미지"> -->
+      </div>
+      <div class="right-section">
+        <!-- <img src="@/assets/img/intro-2.png" alt="저시력 시각장애인이 보는 화면 이미지"> -->
+        <p>비밀번호 입력 없이</p>
+        <p>얼굴인식을 통한</p>
+        <p>간편한 인증</p>
+      </div>
+    </div>
+    <div class="title-bottom">
+      <p>편리한 기능을</p>
+    </div>   
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex; /* Flexbox 레이아웃 사용 */
+  flex-direction: column; /* 자식 요소들을 세로로 배열 */
+  justify-content: center; /* 중앙 정렬 (세로 방향) */
+  align-items: center; /* 중앙 정렬 (가로 방향) */
+  height: 100vh; /* 전체 뷰포트 높이 */
+  text-align: center; /* 텍스트 중앙 정렬 */
+}
+
+.img-section {
+  display: flex;
+  height: 100vh; /* 전체 화면 높이를 채우도록 설정 */
+}
+
+.left-section {
+  flex: 1; /* 부모 컨테이너에 대해 1의 flex 비율을 가짐 */
+  flex-direction: column; /* 자식 요소들을 세로로 배열 */
+  display: flex;
+  justify-content: center; /* 가운데 정렬 */
+  align-items: center; /* 세로 중앙 정렬 */
+  padding: 10px;
+}
+
+.right-section {
+  flex: 1; /* 부모 컨테이너에 대해 1의 flex 비율을 가짐 */
+  flex-direction: column; /* 자식 요소들을 세로로 배열 */
+  display: flex;
+  justify-content: center; /* 가운데 정렬 */
+  align-items: center; /* 세로 중앙 정렬 */
+  padding: 10px;
+}
+</style>
