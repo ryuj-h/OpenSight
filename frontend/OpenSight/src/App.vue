fa1910aa586fcfc51fee19cf2928ed8e0cb7d039
임태수 <ts980730@naver.com>
2024-03-20 10:58:34 +0900

    :wrench: init: FE 프로젝트 폴더 구조 변경

--- frontend/OpenSight/src/App.vue
+++ frontend/OpenSight/src/App.vue
@@ -8,5 +8,13 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
-
+.container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: #1B3C62;
+  overflow: auto;
+}
 </style>
