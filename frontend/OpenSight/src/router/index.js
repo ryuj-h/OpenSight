9bc146c2831b1b281a2b9f2e92bbe797bee0ee6e
임태수 <ts980730@naver.com>
2024-03-20 19:38:10 +0900

    :sparkles: feat:비밀번호변경 페이지 구조

--- frontend/OpenSight/src/router/index.js
+++ frontend/OpenSight/src/router/index.js
@@ -1,10 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import PasswordFindView from '@/views/auth/PasswordFind.vue'
+import PasswordFindFailView from '@/views/auth/PasswordFindFail.vue'
+import PasswordChangeView from '@/views/auth/PasswordChange.vue'
+import PasswordChangeCompleteView from '@/views/auth/PasswordChangeComplete.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {
+      path: '/find-password',
+      name: 'PasswordFind',
+      component: PasswordFindView
+    },
+    {
+      path: '/find-password/fail',
+      name: 'PasswordFindFail',
+      component: PasswordFindFailView
+    },
+    {
+      path: '/password/change',
+      name: 'PasswordChange',
+      component: PasswordChangeView
+    },
+    {
+      path: '/password/change-complete',
+      name: 'PasswordChangeComplete',
+      component: PasswordChangeCompleteView
+    },
   ]
 })
 
