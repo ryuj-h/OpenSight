89da8a37b4438570a05eb44a1a8491228aa5d8a8
임태수 <ts980730@naver.com>
2024-03-20 16:01:40 +0900

    :sparkles: feat:회원가입 완료 페이지까지 구조 완성

--- frontend/OpenSight/src/App.vue
+++ frontend/OpenSight/src/App.vue
@@ -4,7 +4,9 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <div class="container">
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
--- frontend/OpenSight/src/router/index.js
+++ frontend/OpenSight/src/router/index.js
@@ -1,10 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import RegisterView from '@/views/auth/Register.vue'
+import RegisterCompleteView from '@/views/auth/RegisterComplete.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {
+      path: '/register',
+      name: 'Reigster',
+      component: RegisterView
+    },
+    {
+      path: '/register/complete',
+      name: 'RegisterComplete',
+      component: RegisterCompleteView
+    }
   ]
 })
 
