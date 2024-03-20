1ddfb1353c014d220eecc2f041d52ce148912f6d
임태수 <ts980730@naver.com>
2024-03-20 16:54:22 +0900

    :sparkles: feat: 이메일찾기 구조 추가

--- frontend/OpenSight/src/router/index.js
+++ frontend/OpenSight/src/router/index.js
@@ -1,10 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import FindEmail from '@/views/auth/FindEmail.vue'
+import FindEmailComplete from '@/views/auth/FindEmailComplete.vue'
+import FindEmailFail from '@/views/auth/FindEmailFail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {
+      path: '/find-email',
+      name: 'FindEmail',
+      component: FindEmail
+    },
+    {
+      path: '/find-email/complete',
+      name: 'FindEmailComplete',
+      component: FindEmailComplete
+    },
+    {
+      path: '/find-email/fail',
+      name: 'FindEamilFail',
+      component: FindEmailFail
+    }
   ]
 })
 
