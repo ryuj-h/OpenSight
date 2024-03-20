a576a16d2e9353253a3880f0515191f559cc9f1d
임태수 <ts980730@naver.com>
2024-03-20 17:22:09 +0900

    :sparkles: feat: 회원정보수정 구조

--- frontend/OpenSight/src/router/index.js
+++ frontend/OpenSight/src/router/index.js
@@ -1,10 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import EditProfileView from '@/views/profile/EditProfile.vue'
+import EditProfileCompleteView from '@/views/profile/EditProfileComplete.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {
+      path: '/profile/edit',
+      name: 'EditProfile',
+      component: EditProfileView
+    },
+    {
+      path: '/profile/edit-complete',
+      name: 'EditProfileComplete',
+      component: EditProfileCompleteView
+    }
   ]
 })
 
