cf282d59ef42aa2eae64361a4336e569ff71cf07
혜지 <hms7248@naver.com>
2024-03-21 17:54:49 +0900

    :sparkles: feat : chatBot 요청 추가

---
+++ backend/src/main/java/org/example/b104/domain/chatbot/service/ChatBotService.java
@@ -0,0 +1,11 @@
+package org.example.b104.domain.chatbot.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Service
+public class ChatBotService {
+
+
+
+}
