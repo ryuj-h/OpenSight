209ca2060fb48c32b4e3057f12540a428a8c1eae
임태수 <ts980730@naver.com>
2024-03-21 17:51:08 +0900

    :sparkles: feat: 거래내역조회 구조

---
+++ frontend/OpenSight/src/components/layout/Account.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="account">
+    <p>은행명</p>
+    <p>통장이름</p>
+    <p>계좌번호</p>
+    <p>잔액</p>
+  </div>
+</template>
+
+<style scoped>
+.account {
+  background-color: #416692;
+}
+
+p {
+  color: #ffffff;
+}
+</style>
