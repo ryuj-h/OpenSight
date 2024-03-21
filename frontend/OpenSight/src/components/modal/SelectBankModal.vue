ad27caa5d4fd92f90c3da64dde3c0e02a7e98623
임태수 <ts980730@naver.com>
2024-03-21 16:19:30 +0900

    :sparkles: feat: 계좌이체 구조

---
+++ frontend/OpenSight/src/components/modal/SelectBankModal.vue
@@ -0,0 +1,15 @@
+<script setup>
+banks = ['산업은행','기업은행', '한국은행', '국민은행']
+</script>
+
+<template>
+  <div>
+    <div v-for="bank in banks">
+      <button>{{ bank }}</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
