a199108345aacaba74ce719be4df469371c85198
혜지 <hms7248@naver.com>
2024-03-22 13:25:33 +0900

    bug: fix : .xml 추적 금지

--- backend/src/main/java/org/example/b104/domain/chatbot/controller/request/ChatBotTextRequest.java
+++ backend/src/main/java/org/example/b104/domain/chatbot/controller/request/ChatBotTextRequest.java
@@ -1,10 +1,18 @@
 package org.example.b104.domain.chatbot.controller.request;
 
 import lombok.Data;
+import org.example.b104.domain.chatbot.service.command.ChatBotTextCommand;
 
 @Data
 public class ChatBotTextRequest {
 
     private Long number;
     private String tmp;
+
+    public ChatBotTextCommand toChatBotTextCommand() {
+        return ChatBotTextCommand.builder()
+                .number(number)
+                .tmp(tmp)
+                .build();
+    }
 }
