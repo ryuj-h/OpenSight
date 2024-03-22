b1bad044c1de85fe6e669c9e3b52c6708944eda4
임태수 <ts980730@naver.com>
2024-03-22 12:58:07 +0900

    :sparkles: feat:예적금추천페이지 구조

--- frontend/OpenSight/src/router/index.js
+++ frontend/OpenSight/src/router/index.js
@@ -13,6 +13,8 @@ import PasswordFindView from '@/views/auth/PasswordFind.vue'
 import PasswordFindFailView from '@/views/auth/PasswordFindFail.vue'
 import PasswordChangeView from '@/views/auth/PasswordChange.vue'
 import PasswordChangeCompleteView from '@/views/auth/PasswordChangeComplete.vue'
+import SavingsView from '@/views/savings/Savings.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -86,6 +88,11 @@ const router = createRouter({
       path: '/password/change-complete',
       name: 'PasswordChangeComplete',
       component: PasswordChangeCompleteView
+    },
+    {
+      path: '/savings',
+      name: 'Savings',
+      component: SavingsView
     },
   ]
 })
