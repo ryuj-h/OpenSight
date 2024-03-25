0d5caaab8850e556f9ed571b69376ad2eb64546f
임지현 <acubens03@gmail.com>
2024-03-25 10:35:34 +0900

    :rewind: rollback : gradle 오류로 변경사항 복구

--- backend/src/test/java/org/example/b104/B104ApplicationTests.java
+++ backend/src/test/java/org/example/b104/B104ApplicationTests.java
@@ -1,5 +1,6 @@
 package org.example.b104;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 
