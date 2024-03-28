6c76bbf6775bb3ccc0ce6986d33f7da17fac1f28
임태수 <ts980730@naver.com>
2024-03-28 22:13:38 +0900

    :lipstick: feat: css수정

--- frontend/OpenSight/src/views/auth/PasswordChange.vue
+++ frontend/OpenSight/src/views/auth/PasswordChange.vue
@@ -4,82 +4,72 @@
 
 <template>
 <div class="container">
-    
-    <div class="content">
-      <div>&lt; 비밀번호 찾기</div>
-      <form class="form">
-        <label for="password">새로운 비밀번호를 입력하세요</label>
-        <input type="password" id="password" placeholder="비밀번호" required>
-        <p>비밀번호는 다음과 같은 조건을 만족해야 합니다</p>
-        <div class="pwd-check1">
-          <span v-if="isLengthValid" class="checkmark">✔</span>
-          <span v-else>✔</span>
-          <p>비밀번호는 공백없이 10자리 이상 30자 이하여야 합니다</p>
-        </div>
-        <div class="pwd-check2">
-          <span v-if="hasAllRequiredCharacters" class="checkmark">✔</span>
-          <span v-else>✔</span>
-          <p>영어 대문자, 영어 소문자, 특수문자를 최소 한 글자 이상 포함한 문자 조합이어야 합니다.</p>
-        </div>
-        <label for="passwordConfirm">비밀번호를 확인해주세요</label>
-        <input type="password" id="passwordConfirm" placeholder="비밀번호 확인" required>
-        <button type="submit" @click="goComplete">비밀번호 변경</button>
-      </form>
-    </div>
+  <div class="header">
+    <p class="title2">&lt;</p><p class="title2">비밀번호 찾기</p>
   </div>
+  <div class="content">
+    <form class="form">
+      <label for="password"><p class="caption1">새로운 비밀번호를 입력하세요</p></label>
+      <input class="input" type="password" id="password" placeholder="비밀번호" >
+      <p class="caption1">비밀번호는 다음과 같은 조건을 만족해야 합니다</p>
+      <p class="caption2">비밀번호는 공백없이 10자리 이상 30자 이하여야 합니다</p>
+      <p class="caption2">영어 대문자, 영어 소문자, 특수문자를 최소 한 글자 이상 포함한 문자 조합이어야 합니다.</p>
+      <label for="passwordConfirm"><p class="caption1">비밀번호를 확인해주세요</p></label>
+      <input class="input" type="password" id="passwordConfirm" placeholder="비밀번호 확인" >
+    </form>
+    <button class="button" type="submit" @click="goComplete">비밀번호 변경</button>
+  </div>
+</div>
 </template>
 
 <style scoped>
-container {
-  font-family: 'Arial', sans-serif;
-  max-width: 360px;
-  margin: 0 auto;
-  overflow: auto;
+.container {
   display: flex;
   flex-direction: column;
+  background-color: #ffffff;
+}
+.header {
+  display: flex;
+  flex-direction: row;
+  padding-left: 30px;
+  justify-content: left;
+  text-align: left;
 }
 .content {
   padding: 20px;
   background-color: #ffffff;
   border-radius: 10px 10px 0 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 .form {
   display: flex;
   flex-direction: column;
+  justify-content: left;
+  text-align: left;
 }
 .form label {
   margin-top: 20px;
 }
-.form input {
+.input {
+  width: 340px;
+  height: 30px;
   padding: 10px;
-  margin-top: 5px;
-  border: 1px solid #ccc;
+  margin-bottom: 5px;
+  border: 1px solid #dcdfe6;
+  border-radius: 4px;
+}
+.button {
+  margin-top: 200px;
+  width: 327px;
+  height: 45px; 
+  font-size: 16px;
+  font-weight: 500;
+  background-color: #1B3C62;
+  color: #ffffff;
   border-radius: 10px;
-}
-.form small {
-  color: #888;
-  margin-top: 5px;
-}
-.form ul {
-  color: #555;
-  font-size: 0.9em;
-  margin-top: 5px;
-  padding-left: 20px;
-}
-.form button {
-  background-color: #007aff;
-  color: white;
-  padding: 10px;
-  margin-top: 30px;
-  border: none;
-  border-radius: 10px;
-  font-size: 1em;
-}
-.back-button {
-  font-size: 1em;
-  color: #007aff;
-  margin-bottom: 30px;
-  cursor: pointer;
 }
 .checkmark {
   color: green;
@@ -87,5 +77,16 @@ container {
 }
 .pwd-check1 {
   flex-direction: row;
+}
+.title2 {
+  margin-right: 10px;
+}
+
+.caption2 {
+  margin: 0;
+}
+.caption1 {
+  margin-top: 5px;
+  margin-bottom: 15px;
 }
 </style>
---
+++ frontend/OpenSight/src/views/transfer/TransferVerify.vue
@@ -0,0 +1,109 @@
+<script setup>
+import { ref } from 'vue';
+
+const input = ref('');
+
+const appendNumber = (number) => {
+  input.value += number;
+};
+
+const clearInput = () => {
+  input.value = '';
+};
+
+const backspace = () => {
+  input.value = input.value.slice(0, -1);
+};
+
+const confirmInput = () => {
+  // 입력된 값을 확인하는 로직
+  console.log(input.value);
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="numpad-container">
+      <div class="input-display">
+        {{ input }}
+      </div>
+      <div class="numpad">
+        <button v-for="number in 9" :key="number" @click="appendNumber(number)">
+          {{ number }}
+        </button>
+        <button @click="clearInput">C</button> <!-- 전체 삭제 -->
+        <button @click="appendNumber(0)">0</button>
+        <button @click="backspace">&#9003;</button> <!-- 단일 삭제 -->
+      </div>
+      <div class="actions">
+        <button class="cancel" @click="clearInput">취소</button>
+        <button class="confirm" @click="confirmInput">이제</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.container {
+  background-color: #ffffff;
+}
+
+.numpad-container {
+  width: 100%;
+  max-width: 300px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+}
+
+.input-display {
+  width: 100%;
+  border-bottom: 1px solid #ccc;
+  padding: 10px;
+  text-align: center;
+  font-size: 24px;
+}
+
+.numpad {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 5px;
+}
+
+.numpad button {
+  padding: 15px;
+  font-size: 18px;
+  background-color: #f7f7f7;
+  border: 1px solid #ddd;
+  flex: 1;
+}
+
+.numpad button.special {
+  background-color: #dedede; /* 특별한 기능을 가진 버튼의 배경색 */
+}
+
+.actions {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.actions button {
+  padding: 10px 30px;
+  font-size: 16px;
+  border: none;
+  border-radius: 5px;
+}
+
+.cancel {
+  background-color: #e0e0e0;
+}
+
+.confirm {
+  background-color: #1B3C62;
+  color: white;
+}
+</style>
