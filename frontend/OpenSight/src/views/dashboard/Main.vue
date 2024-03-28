f82cf49d64f55a4369cacc06f164339b538a979e
임태수 <ts980730@naver.com>
2024-03-28 11:00:41 +0900

    :art: feat: css수정중

--- frontend/OpenSight/src/views/dashboard/Main.vue
+++ frontend/OpenSight/src/views/dashboard/Main.vue
@@ -1,8 +1,13 @@
 <script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter()
+
+
 </script>
 
 <template>
   <div>
     
+    <button @click="router.push('/account/open/select-bank')">비대면계좌 개설하기</button>
   </div>
 </template>
