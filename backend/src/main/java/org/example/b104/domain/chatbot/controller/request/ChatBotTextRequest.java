4efc26168d3aca27791c9761bc8dab2c99cdb867
혜지 <hms7248@naver.com>
2024-03-30 19:00:01 +0900

    :art: refactor : 챗봇 1번 요청 수정

--- backend/src/main/java/org/example/b104/domain/chatbot/controller/request/ChatBotTextRequest.java
+++ backend/src/main/java/org/example/b104/domain/chatbot/controller/request/ChatBotTextRequest.java
@@ -1,16 +1,19 @@
 package org.example.b104.domain.chatbot.controller.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.example.b104.domain.chatbot.service.command.ChatBotTextCommand;
 
 @Data
 public class ChatBotTextRequest {
 
+    @JsonProperty("command_id")
     private Long command_id;
     private String message;
     private String bank;
     private String account;
     private Long money;
+    @JsonProperty("ischatbot")
     private Long ischatbot;
 
     public ChatBotTextCommand toChatBotTextCommand() {
