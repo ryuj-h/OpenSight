d5fc83b67ecbbc4b682b24b247e0b46b45c08eb0
혜지 <hms7248@naver.com>
2024-03-31 18:34:41 +0900

    :art: refactor : 챗봇 구조 수정 완료

--- backend/src/main/java/org/example/b104/domain/chatbot/controller/response/OpenAccountResponse.java
+++ backend/src/main/java/org/example/b104/domain/chatbot/controller/response/OpenAccountResponse.java
@@ -3,12 +3,12 @@ package org.example.b104.domain.chatbot.controller.response;
 import lombok.Builder;
 import lombok.Getter;
 
-@Builder
 @Getter
-public class BalanceInquiryResponse {
+@Builder
+public class OpenAccountResponse {
     private Integer command_id;
     private String text1;
-    private Integer money;
+    private String url;
     private String text2;
     private Integer ischatbot;
 }
