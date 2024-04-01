d785611e53be3313f0f250fb450bd6a1322c3aca
임태수 <ts980730@naver.com>
2024-04-01 10:04:37 +0900

    :art: feat: transactions css 및 script수정

--- frontend/OpenSight/src/components/modal/PeriodSettingModal.vue
+++ frontend/OpenSight/src/components/modal/PeriodSettingModal.vue
@@ -1,10 +1,30 @@
 <script setup>
+import { ref } from 'vue';
+import { defineEmits } from 'vue';
 
+const emit = defineEmits(['close', 'update'])
+
+const startDate = ref('');
+const endDate = ref('');
+
+const submit = () => {
+  emit('update', { startDate: startDate.value, endDate: endDate.value });
+};
+
+const close = () => {
+  emit('close');
+};
 </script>
 
 <template>
-  <div>
-    
+  <div class="modal">
+    <div class="modal-content">
+      <h3>조회기간 설정</h3>
+      <input type="date" v-model="startDate" />
+      <input type="date" v-model="endDate" />
+      <button @click="submit">확인</button>
+      <button @click="close">취소</button>
+    </div>
   </div>
 </template>
 
