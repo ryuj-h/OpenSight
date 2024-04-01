2733fcd25732bbf026471e8ccb3f019fc75529ab
류진호 <dbwlsgh57@naver.com>
2024-04-01 17:43:23 +0900

    :bug: fix : historyRecord JSON 이름 추가

--- backend/src/main/java/org/example/b104/domain/account/controller/record/InquireAccountHistoryRecord.java
+++ backend/src/main/java/org/example/b104/domain/account/controller/record/InquireAccountHistoryRecord.java
@@ -1,13 +1,16 @@
 package org.example.b104.domain.account.controller.record;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 import lombok.Getter;
+
 
 
 public record InquireAccountHistoryRecord(
 
         int totalCount,
 
+        @JsonProperty("list")
         SingleAccountTransactionHistory[] transactionHistory
 ) {
 }
