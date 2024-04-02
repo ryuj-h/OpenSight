af098021d24b03a8418e6251e41deb24533ec0ad
혜지 <hms7248@naver.com>
2024-04-02 09:43:23 +0900

    :sparkles: feat : 소셜로그인 기능 추가

---
+++ frontend/OpenSight/src/views/dashboard/SocialLogin.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useRouter} from "vue-router";
+const router  = useRouter()
+
+// URL에서 쿼리 스트링 파싱
+const queryParams = new URLSearchParams(window.location.search);
+
+// 필요한 값(예: accessToken) 추출
+const accessToken = queryParams.get('accessToken');
+
+if (accessToken) {
+  sessionStorage.setItem('accessToken', accessToken);
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <p>환영합니다</p>
+    <button class="button" @click="router.push('/main')">시작하기</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
