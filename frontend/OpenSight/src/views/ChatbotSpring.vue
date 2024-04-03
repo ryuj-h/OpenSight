1a3c21535ff5ab43a9132f3d2ddf7af0e5ebada4
류진호 <dbwlsgh57@naver.com>
2024-04-03 23:41:15 +0900

    :sparkles: feat : 챗봇 계좌이체 얼굴인식 추가

--- frontend/OpenSight/src/views/ChatbotSpring.vue
+++ frontend/OpenSight/src/views/ChatbotSpring.vue
@@ -28,7 +28,90 @@ const transferBalance = ref("");
 
 const messageToSend = ref("");
 
-const cameraActivate = ref(false)
+const isCameraReady = ref(false);
+const canvasRef = ref(null);
+const videoRef = ref(null);
+const captureImageFilter = () =>{
+  if (isCameraReady.value === false){
+    setupCamera();
+    alert("카메라 준비 완료 다시 버튼을 눌러주세요");
+    isCameraReady.value = true;
+    return;
+  }
+  else{
+    captureImage();
+    alert("사진 캡쳐 완료");
+
+    const now = new Date();
+    const dateTimeStr = now.toISOString().replace(/[^0-9]/g, '').slice(0, 14);
+    const uniqueFilename = `capturedImage_${dateTimeStr}.jpg`;
+
+    const accessToken = sessionStorage.getItem('accessToken');
+
+    canvasRef.value.toBlob(async (blob) => {
+      const formData = new FormData();
+      formData.append('requestImage', blob, uniqueFilename);
+
+      try {
+        const response = await axios.post(
+            `${import.meta.env.VITE_REST_API}/users/face-auth`,
+            formData,
+            {
+              headers: {
+                'Content-Type': 'multipart/form-data', // Content-Type을 올바르게 설정해야 합니다.
+                'Authorization': `${accessToken}`
+              }
+            }
+        );
+        console.log(response.data);
+        if (response.data.data.result === "success") {
+          alert("얼굴인식 성공");
+
+          cameraActivate.value = false;
+          await accountStore.inquireAccountList();
+
+          console.log(accountStore.myAccountList[0].bankCode);
+          console.log(accountStore.myAccountList[0].accountNo);
+          console.log ("balance : " , transferBalance.value);
+          console.log ("receiverBankCode : " , receiverBankCode.value);
+          console.log ("receiverAccountNo : " , receiverAccountNo.value);
+
+
+          accountStore.selectedBank = receiverBankCode.value;
+          accountStore.accountNumber = receiverAccountNo.value;
+          accountStore.amount = transferBalance.value;
+          accountStore.selectedMyAccountBankCode = accountStore.myAccountList[0].bankCode;
+          accountStore.selectMyAccountNumber = accountStore.myAccountList[0].accountNo;
+          accountStore.accountTransfer();
+          messages.value.push(
+              {
+                message : "입금 완료.",
+                isChatbot : 1
+              }
+          )
+          cameraActivate.value = false;
+
+        } else {
+          alert("얼굴인식 실패");
+          cameraActivate.value = false;
+        }
+      }
+      catch (error) {
+        console.error(error);
+      }
+    }, 'image/jpeg');
+  }
+}
+
+const setupCamera = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    videoRef.value.srcObject = stream;
+    console.error("Setup camera successful");
+  } catch (error) {
+    console.error("Error accessing the camera", error);
+  }
+};
 
 async function sendVoiceToCommand() {
   alert("sendVoiceToCommand");
@@ -82,6 +165,9 @@ const sendButtonClick = () => {
   }
   textMessage.value = "";
 }
+
+
+
 
 watch(()=> store.getSpringResponse, async (command) => {
   switch (command.data.commandId) {
@@ -150,12 +236,6 @@ watch(()=> store.getSpringResponse, async (command) => {
       )
       break;case "102":
 
-      messages.value.push(
-          {
-            message : command.data.answer,
-            isChatbot : 1
-          }
-      );
       if (command.data.answer === "Yes") {
         messages.value.push(
             {
@@ -163,27 +243,7 @@ watch(()=> store.getSpringResponse, async (command) => {
               isChatbot : 1
             }
         )
-        await accountStore.inquireAccountList();
-
-        console.log(accountStore.myAccountList[0].bankCode);
-        console.log(accountStore.myAccountList[0].accountNo);
-        console.log ("balance : " , transferBalance.value);
-        console.log ("receiverBankCode : " , receiverBankCode.value);
-        console.log ("receiverAccountNo : " , receiverAccountNo.value);
-
-
-        accountStore.selectedBank = receiverBankCode.value;
-        accountStore.accountNumber = receiverAccountNo.value;
-        accountStore.amount = transferBalance.value;
-        accountStore.selectedMyAccountBankCode = accountStore.myAccountList[0].bankCode;
-        accountStore.selectMyAccountNumber = accountStore.myAccountList[0].accountNo;
-        accountStore.accountTransfer();
-        messages.value.push(
-            {
-              message : "입금 완료.",
-              isChatbot : 1
-            }
-        )
+        cameraActivate.value = true;
       }
       else {
         messages.value.push(
@@ -213,12 +273,14 @@ watch(()=> store.getSpringResponse, async (command) => {
   </div>
 
   <div v-if="cameraActivate" class="modal">
+    <video ref="videoRef" autoplay style="display:none;"></video>
+    <canvas ref="canvasRef" style="display:none;"></canvas>
     <!-- 여기 밑 클릭 이벤트에 카메라 활성화하는 함수 작성 -->
     <!-- 카메라 활성화하는 함수를 작성했을 때 cameraActivate.value = ture로 바꾸고 -->
     <!-- 얼굴 인식이 끝나면 cameraActivate.value = false로 바꿔야 함 -->
-    <div @click="">
+    <div @click="captureImageFilter">
       <img class="faceid" src="../assets/img/faceid.png" alt="얼굴 인식 버튼">
-    </div> 
+    </div>
   </div>
 
   <div class="content">
