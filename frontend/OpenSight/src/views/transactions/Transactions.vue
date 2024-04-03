9c7f294b69b2f83b8c0f9c1886e257123d8a7a64
Ray Oh <rayoh2098@gmail.com>
2024-04-03 20:54:25 +0900

    :feat: transactions.vue 수정

--- frontend/OpenSight/src/views/transactions/Transactions.vue
+++ frontend/OpenSight/src/views/transactions/Transactions.vue
@@ -2,31 +2,30 @@
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import TransactionList from '@/components/transactinos/TransactionList.vue'
+// import PeriodSettingModal from '@/components/modal/PeriodSettingModal.vue';
 import Account from '@/components/layout/Account.vue'
 import axios from 'axios';
+
+import { useAccountStore } from '@/stores/account'
+
+const store = useAccountStore()
 
 const router = useRouter()
 
 const loading = ref(false);
 const error = ref(null);
-const transactions = ref([]) 
-const apiUrl = 'http://localhost:8080/api/accounts/inquire-account-transaction-history';
+const transactions = ref([])
+// const apiUrl = 'http://localhost:8080/api/accounts/inquire-account-transaction-history';
+const apiUrl = 'https://j10b104.p.ssafy.io/api/accounts/inquire-account-transaction-history'
 const accessToken = sessionStorage.getItem('accessToken');
 
-const requestData = {
-    bankCode: "004",
-    accountNo: "0048656398367274",
-    startDate: "20240101",
-    endDate: "20241231",
-    transactionType: "A",
-    orderByType: "DESC",
-  };
+console.log('!!!', store.myAccountList.value, '!!!')
+console.log('@@@', store.currentIndex.value, '@@@')
+console.log('###', store.myAccountList[store.currentIndex.value], '###')
 
-  onMounted( async () => {
-    const aaa = await requestTransactionData(requestData);
-    console.log("transactions", aaa);
-})
+const requestData = ref(store.myAccountList[store.currentIndex.value])
 
+// 최근 거래내역조회를 가져오는 함수
 const requestTransactionData = async (requestData) =>{
   try {
     await axios({
@@ -41,8 +40,8 @@ const requestTransactionData = async (requestData) =>{
       console.log("Transaction data requested successfully:", res.data);
       transactions.value = res.data.data.rec.list;
     })
-    
-    
+
+
   } catch (err) {
     console.error("Failed to request transaction data:", err);
     throw err; // Rethrow the error to handle it where this function is called
@@ -50,7 +49,11 @@ const requestTransactionData = async (requestData) =>{
 }
 
 
-
+// 페이지를 불러오면서 해당 계좌에 있는 거래내역 가져오기
+onMounted( async () => {
+  const aaa = await requestTransactionData(requestData);
+  console.log("transactions", aaa);
+})
 </script>
 
 
@@ -63,7 +66,9 @@ const requestTransactionData = async (requestData) =>{
     <div class="content">
       <Account class="account" />
       <button class="button" @click="">이체하기</button>
+      <!-- <button class="">조회기간설정</button> -->
       <TransactionList :transactions="transactions" />
+      <!-- <DateRangeModal v-if="isModalOpen" @update="updateDateRange" @close="closeModal" /> -->
     </div>
   </div>
 </template>
