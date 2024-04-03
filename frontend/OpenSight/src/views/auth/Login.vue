94f9d5d36bdd569a53336feaeb2531fbf85a5130
류진호 <dbwlsgh57@naver.com>
2024-04-03 17:28:53 +0900

    :bug: fix : 소셜로그인 수정

--- frontend/OpenSight/src/views/auth/Login.vue
+++ frontend/OpenSight/src/views/auth/Login.vue
@@ -87,7 +87,7 @@ const logIn = function () {
 
 
 function naverSocialLogin() {
-  window.location.href = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=***REMOVED***&redirect_uri=http://192.168.31.25:8080/login/oauth2/code/naver'
+  window.location.href = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=***REMOVED***&redirect_uri=https://j10b104.p.ssafy.io/api/login/oauth2/code/naver'
 }
 </script>
 
